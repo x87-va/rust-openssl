6d2eef0907bd5f6340a43c6b6727a70447fc0ca9
Vitaliy Levashov <vitaliy.levashov@gmail.com>
2020-08-06 12:06:25 +0300

    Implemented Engine methods: ctrl_cmd_string, set_default, get_name

--- openssl-sys/src/engine.rs
+++ openssl-sys/src/engine.rs
@@ -2,23 +2,35 @@ use libc::*;
 
 use *;
 
+pub const ENGINE_METHOD_NONE: c_uint 			= 0x0000;
+pub const ENGINE_METHOD_RSA: c_uint             = 0x0001;
+pub const ENGINE_METHOD_DSA: c_uint             = 0x0002;
+pub const ENGINE_METHOD_DH: c_uint              = 0x0004;
+pub const ENGINE_METHOD_RAND: c_uint            = 0x0008;
+pub const ENGINE_METHOD_CIPHERS: c_uint         = 0x0040;
+pub const ENGINE_METHOD_DIGESTS: c_uint         = 0x0080;
+pub const ENGINE_METHOD_PKEY_METHS: c_uint      = 0x0200;
+pub const ENGINE_METHOD_PKEY_ASN1_METHS: c_uint = 0x0400;
+pub const ENGINE_METHOD_EC: c_uint              = 0x0800;
+pub const ENGINE_METHOD_ALL: c_uint 			= 0xFFFF;
+
 extern "C" {
 	
 	pub fn ENGINE_by_id(id: *const c_char) -> *mut ENGINE;
 
-	// pub fn ENGINE_init(engine: *mut ENGINE) -> c_int;
+	pub fn ENGINE_init(engine: *mut ENGINE) -> c_int;
 	
-	// pub fn ENGINE_finish(engine: *mut ENGINE) -> c_int;
+	pub fn ENGINE_finish(engine: *mut ENGINE) -> c_int;
 	
 	pub fn ENGINE_free(engine: *mut ENGINE) -> c_int;
 
 	pub fn ENGINE_get_id(engine: *const ENGINE) -> *const c_char;
 	
-	// pub fn ENGINE_get_name(engine: *mut ENGINE) -> *const c_char;
+	pub fn ENGINE_get_name(engine: *const ENGINE) -> *const c_char;
  
-	// pub fn ENGINE_set_default(engine: *mut ENGINE, flags: c_uint) -> c_int;
+	pub fn ENGINE_set_default(engine: *mut ENGINE, flags: c_uint) -> c_int;
 
-	// pub fn ENGINE_ctrl_cmd_string(engine: *mut ENGINE, cmd_name: *const c_char, arg: *const c_char, cmd_optional: c_uint) -> c_int;
+	pub fn ENGINE_ctrl_cmd_string(engine: *mut ENGINE, cmd_name: *const c_char, arg: *const c_char, cmd_optional: c_int) -> c_int;
 
 	// pub fn ENGINE_load_private_key(engine: *mut ENGINE, key_id: *const c_char, ui_method: *mut UI_METHOD, callback_data: *mut c_void) -> *mut EVP_PKEY;
 
--- openssl/src/engine.rs
+++ openssl/src/engine.rs
@@ -1,5 +1,7 @@
 use std::ffi::{CString, CStr};
 
+use libc::{c_uint, c_int};
+
 use ffi;
 
 use {cvt_p, cvt};
@@ -18,10 +20,11 @@ pub struct Engine(*mut ffi::ENGINE);
 impl Engine {
 
     pub fn by_id(engine_id: &str) -> Result<Engine, ErrorStack> {
+        let id = CString::new(engine_id).unwrap();
+        
         unsafe {
             ffi::init();
 
-            let id = CString::new(engine_id).unwrap();
             cvt_p(ffi::ENGINE_by_id(id.as_ptr())).map(Engine)
         }
     }
@@ -33,7 +36,41 @@ impl Engine {
         }
     }
 
-    pub fn free(&self) -> Result<(), ErrorStack> {
+    pub fn get_name(&self) -> String {
+        unsafe {
+            let name = ffi::ENGINE_get_name(self.0);
+            CStr::from_ptr(name).to_string_lossy().to_string()
+        }
+    }
+
+    pub fn set_default(&mut self, flags: EngineMethod) -> Result<(), ErrorStack> {
+        unsafe {
+            cvt(ffi::ENGINE_set_default(self.0, flags.0)).map(|_| ())
+        }        
+    }
+
+    pub fn init(&mut self) -> Result<(), ErrorStack> {
+        unsafe {
+            cvt(ffi::ENGINE_init(self.0)).map(|_| ())
+        }
+    }
+
+    pub fn ctrl_cmd_string(&mut self, command: &str, arg: &str, cmd_optional: i32) -> Result<(), ErrorStack> {
+        let cmd_name = CString::new(command).unwrap();
+        let cmd_arg = CString::new(arg).unwrap();
+
+        unsafe {
+            cvt(ffi::ENGINE_ctrl_cmd_string(self.0, cmd_name.as_ptr(), cmd_arg.as_ptr(), cmd_optional as c_int)).map(|_| ())
+        }
+    }   
+
+    pub fn finish(&mut self) -> Result<(), ErrorStack> {
+        unsafe {
+            cvt(ffi::ENGINE_finish(self.0)).map(|_| ())
+        }
+    }
+
+    pub fn free(&mut self) -> Result<(), ErrorStack> {
         unsafe {
             cvt(ffi::ENGINE_free(self.0)).map(|_| ())
         }
@@ -41,17 +78,61 @@ impl Engine {
 
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct EngineMethod(c_uint);
+
+impl EngineMethod {
+
+    pub const NONE: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_NONE);
+    
+    pub const RSA: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_RSA);
+    
+    pub const DSA: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_DSA);
+
+    pub const DH: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_DH);
+    
+    pub const RAND: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_RAND);
+
+    pub const CIPHERS: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_CIPHERS);
+
+    pub const DIGESTS: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_DIGESTS);
+
+    pub const PKEY_METHS: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_PKEY_METHS);
+
+    pub const PKEY_ASN1_METHS: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_PKEY_ASN1_METHS);
+
+    pub const PKEY_ASN1_EC: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_EC);
+
+    pub const ALL: EngineMethod = EngineMethod(ffi::ENGINE_METHOD_ALL);
+}
+
 #[cfg(test)]
 mod tests {
-    use super::Engine;
+    use super::{Engine, EngineMethod};
 
     #[test]
     fn test_engine_by_id() {
-        let result = Engine::by_id("rdrand");
+        const ENGINE_ID: &str = "rdrand";
+
+        let result = Engine::by_id(ENGINE_ID);
         assert!(result.is_ok());
 
-        // let engine = result.unwrap();
-        // let result2 = engine.free();
-        // assert!(result2.is_ok());
+        let mut engine = result.unwrap();
+
+        assert_eq!(engine.get_id(), ENGINE_ID);
+
+        println!("Engine name: {:?}", engine.get_name());
+
+        let mut result2 = engine.init();
+        assert!(result2.is_ok());
+
+        result2 = engine.set_default(EngineMethod::ALL);
+        assert!(result2.is_ok());
+
+        result2 = engine.finish();
+        assert!(result2.is_ok());
+
+        result2 = engine.free();
+        assert!(result2.is_ok());
     }
 }
